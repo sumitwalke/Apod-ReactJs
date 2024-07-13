7824b6f05c09330f6f2d0bbf082ef7ace16c58bf
sumitwalke <sumitwalke123@gmail.com>
2024-07-14 02:06:26 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
+import logo from './logo.svg';
 import './App.css';
-import Sidebar from './components/Sidebar';
-import Main from './components/Main';
-import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      {/* <Sidebar/>
-      <Main/>
-      <Footer/> */}
+      
     </div>
   );
 }
